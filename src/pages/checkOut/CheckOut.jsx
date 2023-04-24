610d69762cd576b919dccad2584087285440192d
dungnguyendev <dungnguyentien.dev@gmail.com>
2023-04-24 15:30:23 +0700

    UP

--- src/pages/checkOut/CheckOut.jsx
+++ src/pages/checkOut/CheckOut.jsx
@@ -23,8 +23,8 @@ const CheckOut = () => {
         // Load ds ghế đặt từ server
         connection.on("loadDanhSanhGheDaDat", (dsGheKhachDat) => {
             console.log("file: CheckOut.jsx:25 ~ connection.on ~ dsGheDangDatReturn:", dsGheKhachDat)
-            
         })
+        // dung checkoIUT
 
     }, [dispatch])
     const { thongTinPhim, danhSachGhe } = chiTietPhongVe
