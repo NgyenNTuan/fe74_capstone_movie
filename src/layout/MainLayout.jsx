ec9b5eabc345145b85f737bede4be9363223b794
Nguyen Ngoc Tuan <godzillated95@gmail.com>
2023-04-26 22:32:49 +0700

    fit main layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
-import "./../pages/checkOut/CheckOut.css";
 const MainLayout = () => {
    return (
       <div className="MainLayout flex flex-col h-full">
          <Header />
 
-         <div className="cover z-10 "></div>
          <div className="MainContent flex-1 min-h-screen z-20 ">
             <Outlet />
          </div>
